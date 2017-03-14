a4952fc9d10a9c9b21c7de6f5b2b585d4fc08662
Sandeep Kumar Singh <singh_sandeep1998@hotmail.com>
2017-03-14 20:12:25 +0000

    Added modularity to admin functions

---
+++ admin.js
@@ -0,0 +1,62 @@
+var uuid = require("node-uuid");
+var _ = require("lodash");
+var rooms = require("./data/rooms.json");
+
+module.exports = function (app) {
+
+    app.get('/admin/rooms', function (req, res) {
+        res.render("rooms", {
+            title: "Admin Rooms",
+            rooms: rooms
+        });
+    });
+
+    app.get('/admin/rooms/add', function (req, res) {
+        res.render("add", {title: "Add Rooms"});
+    });
+
+    app.post('/admin/rooms/add', function (req, res) {
+        var room = {
+            name: req.body.name,
+            id: uuid.v4()
+        };
+
+        rooms.push(room);
+
+        res.redirect("/admin/rooms");
+    });
+
+    app.get('/admin/rooms/edit/:id', function (req, res) {
+        var roomId = req.params.id;
+
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+
+        res.render("edit", {title: "Edit Rooms", room});
+    });
+
+    app.post('/admin/rooms/edit/:id', function (req, res) {
+        var roomId = req.params.id;
+
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+
+        room.name = req.body.name;
+
+        res.redirect("/admin/rooms");
+    });
+
+    app.get('/admin/rooms/delete/:id', function (req, res) {
+        var roomId = req.params.id;
+
+        rooms = rooms.filter(r => r.id !== roomId);
+
+        res.redirect("/admin/rooms");
+    });
+}
--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var app = express();
-var rooms = require("./data/rooms.json");
+
 var bodyParser = require("body-parser");
-var uuid = require("node-uuid");
-var _ = require("lodash");
+
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
@@ -16,61 +15,8 @@ app.get('/', function (req, res) {
     res.render("index", {title: "Home"});
 });
 
-app.get('/admin/rooms', function (req, res) {
-    res.render("rooms", {
-        title: "Admin Rooms",
-        rooms: rooms
-    });
-});
-
-app.get('/admin/rooms/add', function (req, res) {
-    res.render("add", {title: "Add Rooms"});
-});
-
-app.post('/admin/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
-
-    rooms.push(room);
-
-    res.redirect("/admin/rooms");
-});
-
-app.get('/admin/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-
-    var room = _.find(rooms, r => r.id === roomId);
-    if(!room){
-        res.sendStatus(404);
-        return;
-    }
-
-    res.render("edit", {title: "Edit Rooms", room });
-});
-
-app.post('/admin/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-
-    var room = _.find(rooms, r => r.id === roomId);
-    if(!room){
-        res.sendStatus(404);
-        return;
-    }
-
-    room.name = req.body.name;
-
-    res.redirect("/admin/rooms");
-});
-
-app.get('/admin/rooms/delete/:id', function (req, res) {
-    var roomId = req.params.id;
-
-    rooms = rooms.filter(r => r.id !== roomId);
-
-    res.redirect("/admin/rooms");
-});
+var admin = require("./admin");
+admin(app);
 
 app.listen(3000, function () {
     console.log('Chat app listening on port 3000!');
