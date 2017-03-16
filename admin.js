dddba56521119d40fe69b63506f4e6c9345b26e3
Sandeep Kumar Singh <singh_sandeep1998@hotmail.com>
2017-03-16 21:23:53 +0000

    Router route used for better code maintainenance and removed redundancy

--- admin.js
+++ admin.js
@@ -13,14 +13,15 @@ router.get('/rooms', function (req, res) {
     });
 });
 
-router.get('/rooms/add', function (req, res) {
-    res.render("add", {title: "Add Rooms"});
-});
+router.route('/rooms/add')
+    .get(function (req, res) {
+        res.render("add", {title: "Add Rooms"});
+    })
 
-router.post('/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
+    .post(function (req, res) {
+         var room = {
+             name: req.body.name,
+            id: uuid.v4()
     };
 
     rooms.push(room);
@@ -28,25 +29,26 @@ router.post('/rooms/add', function (req, res) {
     res.redirect(req.baseUrl + "/rooms");
 });
 
-router.get('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
+router.route('/rooms/edit/:id')
+    .get(function (req, res) {
+        var roomId = req.params.id;
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
 
-    res.render("edit", {title: "Edit Rooms", room});
-});
+        res.render("edit", {title: "Edit Rooms", room});
+    })
 
-router.post('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
+    .post(function (req, res) {
+         var roomId = req.params.id;
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
+        var room = _.find(rooms, r => r.id === roomId);
+         if (!room) {
+            res.sendStatus(404);
+            return;
     }
 
     room.name = req.body.name;
