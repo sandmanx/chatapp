b04bc3f3750f5afd3d339323d2c89f94ec246673
Sandeep Kumar Singh <singh_sandeep1998@hotmail.com>
2017-03-31 01:49:21 +0000

    Messages for rooms added

--- api.js
+++ api.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var rooms = require("./data/rooms.json");
+var messages = require("./data/messages.json");
+var _ = require("lodash");
 
 var router = express.Router();
 module.exports = router;
 
 router.get("/rooms", function(req, res){
     res.json(rooms);
+});
+
+router.get("/rooms/:roomId/messages", function(req, res){
+    var roomId = req.params.roomId;
+
+    var roomMessages = messages
+        .filter(m => m.roomId == roomId);
+
+    var room = _.find(rooms, r => r.id === roomId);
+    if (!room) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json({
+        room: room,
+        messages: roomMessages
+    })
 });
